63864555a09d475f20e205c8fef9b9de7935507c
cristian1clj <cristian1clj@gmail.com>
2022-08-20 18:55:16 -0500

    discount_coupons completed

--- procentaje_y_descuentos/discount.js
+++ procentaje_y_descuentos/discount.js
@@ -1,24 +1,42 @@
 const inputPrice = document.getElementById('price');
-const inputDiscount = document.getElementById('discount');
+const inputCoupon = document.getElementById('coupon');
 const equalsButton = document.getElementById('equalsButton');
 const finalPrice = document.getElementById('finalPrice');
+
+
+class Coupon{
+    constructor(code, discount) {
+        this.code = code;
+        this.discount = discount;
+    }
+}
+
+const couponList = [];
+couponList.push(
+    new Coupon('ThisIsACode', 25), 
+    new Coupon('fishCanFly', 50), 
+    new Coupon('100%', 15), 
+    new Coupon('ThisIsNotACode', 100)
+    );
+
 
 equalsButton.addEventListener('click', generateDiscountedPrice);
 
 function generateDiscountedPrice() {
     const price = inputPrice.value;
-    const discount = inputDiscount.value;
+    const coupon = inputCoupon.value;
 
-    if (!price || !discount) {
+    if (!price || !coupon) {
         finalPrice.innerText = 'Debes ingresar un valor para el precio y para el descuento';
         return;
     }
 
-    if (discount > 100) {
-        finalPrice.innerText = 'El valor del descuento debe ser menor de 100%';
-        return;
-    }
+    const indexCoupon = couponList.find(element => element.code == coupon);
+    if (indexCoupon) {
+        let result = (price * (100 - indexCoupon.discount)) / 100;
+        finalPrice.innerText = `$${result}`;
 
-    let result = (price * (100 - discount)) / 100;
-    finalPrice.innerText = `$${result}`;
+    } else {
+        finalPrice.innerText = 'Codigo de cupon no valido D:';
+    }
 }
