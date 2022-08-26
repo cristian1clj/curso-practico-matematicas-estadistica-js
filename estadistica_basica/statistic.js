953a57fb6c135868044acc805ff7511cd4a27346
cristian1clj <cristian1clj@gmail.com>
2022-08-26 11:39:33 -0500

    median found and list sorted

--- estadistica_basica/statistic.js
+++ estadistica_basica/statistic.js
@@ -1,13 +1,46 @@
-list = [400, 450, 540, 700, 701];
+list = [400, 450, 515, 370, 410, 540, 700, 701, 80];
 
-function calculateAverage(arr) {
+// Funcionalidades
+function isPair(arr) {
+    return !(arr.length % 2);
+}
+
+
+// Calcular media
+function calculateAverageForeach(arr) {
     let result = 0;
 
     arr.forEach(element => {
         result += element;
     });
-
     result /= arr.length;
 
-    console.log(`Resultado = ${result}`);
+    return result;
+}
+
+function calculateAverageReduce(arr) {
+    const add = arr.reduce((av, nv) => av + nv);
+    let result = add / arr.length;
+
+    return result;
+}
+
+
+// Calcular mediana
+function calculateMedian(arr) {
+    const sortArr = arr.sort((av, nv) => av - nv);
+    let median = 0;
+
+    if (isPair(sortArr)) {
+        let half1 = sortArr[(sortArr.length / 2) - 1];
+        let half2 = sortArr[sortArr.length / 2];
+
+        median = calculateAverageReduce([half1, half2]);
+
+    } else {
+        let half = Math.round(sortArr.length / 2) - 1;
+        median = sortArr[half];
+    }
+
+    return median;
 }
